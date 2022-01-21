7b8b67cbfff78a6f79e5c411730694a23564b0f7
Rafael Inácio <53630208+inacio0196@users.noreply.github.com>
2022-01-20 22:21:50 -0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,3 +1,11 @@
 export const filterObject = (obj: object, callback: (_val: any, _key: string) => boolean): object => {
   return Object.fromEntries(Object.entries(obj).filter(([key, val]) => callback(val, key)));
 }
+
+export const convertBase64 = async (file: File) => {
+  return new Promise((resolver) => {
+    const fileReader = new FileReader()
+    fileReader.readAsDataURL(file)
+    fileReader.onload = () => resolver(fileReader.result)
+  })
+}
